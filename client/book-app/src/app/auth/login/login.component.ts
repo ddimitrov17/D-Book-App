e029a2fe96213b66ae6cda46f66b81b3b12f7f23
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-11-29 16:33:09 +0200

    login and signup

--- client/book-app/src/app/auth/login/login.component.ts
+++ client/book-app/src/app/auth/login/login.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  @ViewChild('LoginForm') form: NgForm | undefined;
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  loginSubmitHandler() {
+    const form = this.form!;
+    if (form.invalid) {
+      console.log('This form is invalid!');
+      return;
+    }
+    const formData = form.value;
+
+    this.http.post('http://localhost:5000/api/auth/login', formData, { withCredentials: true }).subscribe({
+      next: (response) => {
+        console.log('Login successful:', response); //TODO: Remove
+
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        console.error('Login failed:', err); //TODO: Add Error Handling
+      },
+    });
+
+  }
 }
--- client/book-app/src/app/auth/register/register.component.ts
+++ client/book-app/src/app/auth/register/register.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule], 
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css'],
+  standalone: true,
 })
 export class RegisterComponent {
+  @ViewChild('RegisterForm') form: NgForm | undefined;
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  registerSubmitHandler() {
+    const form = this.form!;
+    if (form.invalid) {
+      console.log('This form is invalid!');
+      return;
+    }
+    const formData = form.value;
+
+    this.http.post('http://localhost:5000/api/auth/signup', formData, { withCredentials: true }).subscribe({
+      next: (response) => {
+        console.log('Registration successful:', response); //TODO: Remove
+        this.router.navigate(['/home']);
+      },
+      error: (err) => {
+        console.error('Registration failed:', err); //TODO: Add Error Handling
+      },
+    });
+
+  }
 }
