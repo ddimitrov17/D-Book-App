ce5714f5299200d57144807d7b486ca9c04d2f5a
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-12-06 16:47:57 +0200

    reviews in user profile added

--- client/book-app/src/app/personal-profile/personal-profile.component.ts
+++ client/book-app/src/app/personal-profile/personal-profile.component.ts
@@ -9,28 +9,56 @@ interface User {
   id: string;
 }
 
+interface Review {
+  book_title: string;
+  book_author: string;
+  book_photo: string;
+  review_content: string;
+  creator_id: string;
+  id: string;
+}
+
 @Component({
   selector: 'app-personal-profile',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './personal-profile.component.html',
   styleUrls: ['./personal-profile.component.css']
 })
 export class PersonalProfileComponent implements OnInit {
   user: User | null = null;
+  reviews: Review[] = [];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    // Fetch user data
     this.http
       .get<User>('http://localhost:5000/api/auth/validate', { withCredentials: true })
       .subscribe({
         next: (response) => {
           this.user = response;
-          // console.log(this.user.full_name)
+          this.fetchUserReviews();
         },
         error: (err) => {
           console.error('Error fetching user data:', err);
         },
       });
+  }
+
+  private fetchUserReviews() {
+    if (this.user) {
+      this.http
+        .get<Review[]>(`http://localhost:5000/api/reviews/get-reviews-of-user`, { withCredentials: true })
+        .subscribe({
+          next: (reviews) => {
+            this.reviews = reviews;
+            console.log('User reviews:', this.reviews);
+          },
+          error: (err) => {
+            console.error('Error fetching reviews:', err);
+          },
+        });
+    }
   }
 }
