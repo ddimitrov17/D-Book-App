01769ab091b1516b290d026e9ef07c09e59c4a99
Dimitar Dimitrov <dimitardimitrov0317@gmail.com>
2024-12-09 11:28:12 +0200

    edit functionality

--- client/book-app/src/app/feed/review-details/review-details.component.ts
+++ client/book-app/src/app/feed/review-details/review-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 interface Review {
   book_title: string;
@@ -12,34 +13,36 @@ interface Review {
   id: string;
   creator: {
     username: string;
-  }
+  };
 }
 
 @Component({
   selector: 'app-review-details',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './review-details.component.html',
   styleUrls: ['./review-details.component.css']
 })
 export class ReviewDetailsComponent implements OnInit {
   review: Review | null = null;
   isEditModalOpen = false;
-  isDeleteModalOpen = false
+  isDeleteModalOpen = false;
+  editedReviewContent = '';
 
   constructor(
     private route: ActivatedRoute,
     private http: HttpClient
   ) {}
 
-  
   openEditModal() {
+    if (this.review) {
+      this.editedReviewContent = this.review.review_content; 
+    }
     this.isEditModalOpen = true;
   }
 
   closeEditModal() {
     this.isEditModalOpen = false;
   }
-
 
   openDeleteModal() {
     this.isDeleteModalOpen = true;
@@ -49,16 +52,13 @@ export class ReviewDetailsComponent implements OnInit {
     this.isDeleteModalOpen = false;
   }
 
-
   ngOnInit() {
     this.route.params.subscribe(params => {
       const reviewId = params['id'];
-      // Add endpoint to fetch single review
-      this.http.get<Review>(`http://localhost:5000/api/reviews//get-review/${reviewId}`, { withCredentials: true })
+      this.http.get<Review>(`http://localhost:5000/api/reviews/get-review/${reviewId}`, { withCredentials: true })
         .subscribe({
           next: (review) => {
             this.review = review;
-            console.log(this.review)
           },
           error: (err) => {
             console.error('Error fetching review:', err);
@@ -66,4 +66,21 @@ export class ReviewDetailsComponent implements OnInit {
         });
     });
   }
+
+  editReview() {
+    if (this.review) {
+      const updatedReview = { review_content: this.editedReviewContent }; 
+      this.http.put(`http://localhost:5000/api/reviews/update-review/${this.review.id}`, updatedReview, { withCredentials: true })
+        .subscribe({
+          next: () => {
+            this.review!.review_content = this.editedReviewContent; 
+            this.isEditModalOpen = false;
+          },
+          error: (err) => {
+            console.error('Error updating review:', err);
+          }
+        });
+    }
+  }
 }
+
